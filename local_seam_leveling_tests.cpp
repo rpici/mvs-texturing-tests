a4598a95f9ea2d4d2a9e519beb6c5da78014b5f2
rpici <robert@ici1.com>
2019-03-08 14:57:44 -0800

    Add failing test stub for local_seam_leveling

---
+++ local_seam_leveling_tests.cpp
@@ -0,0 +1,16 @@
+#include <catch.hpp>
+
+SCENARIO( "local seam leveling test 1", "[local_seam_leveling]" )
+{
+    GIVEN( "some given" )
+    {
+        WHEN( "something happens" )
+        {
+            THEN( "the results should be as expected" )
+            {
+                REQUIRE( false );
+            }
+        }
+    }
+}
+
